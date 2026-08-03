0937243507f664c0444bd6c3a0797abdb546692b
sariatislam4141-bit <sariatislam4141@gmail.com>
2026-08-03 18:56:38 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,14 @@
 import os
-import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🤖 Sariat AI Bot সফলভাবে চালু হয়েছে!")
 
-async def main():
-    app = Application.builder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    await app.run_polling()
+app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run_polling()
