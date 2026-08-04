00120af9ce7e355721f71c4fd665ea4e03b4831c
sariatislam4141-bit <sariatislam4141@gmail.com>
2026-08-04 12:47:58 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -206,7 +206,6 @@ async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except Exception as e:
         await update.message.reply_text(f"❌ Error: {e}")
     async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
-   
     if not context.args:
         await update.message.reply_text(
             "🖼️ ব্যবহার:\n/image একটি সুন্দর পাহাড়"
